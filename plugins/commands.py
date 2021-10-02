f1c9ff718d00a4079785ef65d9c4854fa701769a
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-02 13:59:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                   
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/BOTS_GARAGE"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/BOTS_ASK")
                 ]
             ]
         ),
@@ -264,8 +264,8 @@ async def about(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                   
+              
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
